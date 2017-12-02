0f8729053fb07087bad6663abe5360808da6659e
nikitas89 <cvnikita1@gmail.com>
2017-12-03 02:45:38 +0800

    added total cart quantity

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
-
 class Cart extends Component {
+checkout = ()=>{
+console.log('checkout')
+}
+
 
   render() {
     return (
@@ -9,12 +12,15 @@ class Cart extends Component {
         <div>
           Cart Items:
         {this.props.cart.length > 0 ? this.props.cart.map((item) => {
-          return <p>{item.name}{ <span> {item.quantity} </span> }{<span> {item.quantity*item.price} </span>} </p> }) : <p>Empty</p>}
+          return <p>{item.name}{ <span> {item.quantity} </span> }{<span> {parseFloat(Math.round(item.quantity*item.price * 100) / 100).toFixed(2)} </span>} </p> }) : <p>Empty</p>}
         </div>
+        Total : {this.props.total}
+        <button type="button" name="checkoutBtn" onClick={this.checkout}>Checkout</button>
       </div>
-
     );
   }
+
+
 }
 
 export default Cart;
--- src/components/HawkerPage.js
+++ src/components/HawkerPage.js
@@ -4,10 +4,12 @@ import Cart from  './Cart'
 import '../App.css';
 class HawkerPage extends Component {
   state = {
+    //load items from db
   items:  [{id: 1, name: "BASIL LEAF RICE", price: 4.50},
             {id: 2, name: "Epic Apple", price: 3.29},
             {id: 3, name: "Awesome Grape", price: 7.49}],
-  cart: []
+  cart: [],
+  total:''
   }
 
   addToCart =  (item) =>{
@@ -21,30 +23,39 @@ class HawkerPage extends Component {
       } else {
         return cartItem;
       }
-    });
+    })
 
     if (!found) { updatedCart.push({id: item.id, name: item.name, price: item.price, quantity: 1, key:item.id}) }
 
+    let totalNum = 0
+    updatedCart.forEach((item)=>{
+        totalNum += item.quantity*item.price
+      })
+      totalNum = parseFloat(Math.round(totalNum * 100) / 100).toFixed(2);
+
+      console.log("totalNum",totalNum )
+      this.setState({
+        total: totalNum
+      })
+
     this.setState({
       cart: updatedCart
-    });
+    })
   }
-
   render() {
     return (
       <div>
         <header className="App-header">
-          <h1>Sisaket Thai</h1>
+          <h4>Sisaket Thai</h4>
           <img className="hero-image" src="https://www.whyq.sg/images?src=https://s3-ap-southeast-1.amazonaws.com/whyqsg/uploads/stalls/b5e0b7c0ca47415723b28f2d94f9877e.PNG&h=356&w=640&zc=" alt=""/>
           <h3>Menu:</h3>
         </header>
         <div >
-          {/* {if this.state.cart.length>0 {
-            return
-          }} */}
-          <Cart cart={this.state.cart} />
-        </div>
+          {this.state.cart.length>0 &&
+            <Cart cart={this.state.cart} total={this.state.total}/>
 
+          }
+        </div>
         <div className="menu-items-list">
           {this.state.items.map((item, index) => {
             return <MenuItem key={index} item={item} addToCart={this.addToCart} />
